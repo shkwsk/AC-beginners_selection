9544296f31d193d0bcb50dc4814065c5861104ab
shkwsk <ren7link@gmail.com>
2020-04-26 17:06:47 +0900

    accept ABC085C

--- python3/ABC085C.py
+++ python3/ABC085C.py
@@ -1,24 +1,17 @@
+def f(N,Y):
+    for i in range(N+1):
+        for j in range(N+1):
+            k=N-i-j
+            if k < 0:
+                break
+            if i+j+k == N and 10000*i + 5000*j + 1000*k == Y:
+                x,y,z = i,j,k
+                return x,y,z
+    return -1,-1,-1
+
 def main():
-    N, Y = [int(x) for x in input().split()]
-    tmp = 0
-    x, y, z = 0, 0, 0
-    for i in range(N):
-        if tmp + 10000 <= Y:
-            tmp += 10000
-            x += 1
-    print(tmp,x,y,z)
-    for i in range(N - x):
-        if tmp + 5000 <= Y:
-            tmp += 5000
-            y += 1
-    print(tmp,x,y,z)
-    for i in range(N - x - y):
-        if tmp + 1000 <= Y:
-            tmp += 1000
-            z += 1
-    print(tmp,x,y,z)
-    if not tmp == Y:
-        x, y, z = -1, -1, -1
+    N,Y = [int(x) for x in input().split()]
+    x,y,z = f(N,Y)
     print(x,y,z)
 
 if __name__ == '__main__':
